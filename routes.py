35553e86512af722614f2d8a5d36dd5e8456ee69
[K1n9sley] <[xiongjin@ucsb.edu]>
2019-02-04 22:50:12 -0800

    GS temp

--- routes.py
+++ routes.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/calendar")
+def calendar():
+    return render_template("index1.html")
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
